443825489bace22e8b40588c4f698806ee3c18ee
thiagosullivan <thiagosullivan12@gmail.com>
2021-04-30 03:47:25 -0300

    First commit, making the header of facebook

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVwMLEyyZpYquTR9TS5Neg_Nr4nF6EAhI",
+  authDomain: "facebook-clone-b50b3.firebaseapp.com",
+  projectId: "facebook-clone-b50b3",
+  storageBucket: "facebook-clone-b50b3.appspot.com",
+  messagingSenderId: "919873240296",
+  appId: "1:919873240296:web:b63dab1e3270d6851f9d5d",
+  measurementId: "G-N3NTMKPQW2"
+};
